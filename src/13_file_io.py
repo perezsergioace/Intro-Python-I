859920fcace6621a054845c340edae924c89c66e
Sergio Perez <perez.sergio.ace@gmail.com>
2020-04-07 22:43:17 -0400

    completed the file_io.py assignment

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,12 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+# f = open('./foo.txt', 'r')
+# print(f.read())
+# f.close()
+
+with open('foo.txt') as file:
+    print(file.read())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +23,21 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+# f = open("bar.txt", "a")
+# f.write('First Line \nSecond Line\nThird Line')
+# f.close()
+
+# with open('bar.txt') as new_file:
+#     print(new_file.read())
+
+random_text = """
+First Line
+Second Line
+Third Line
+"""
+
+with open('bar.txt', 'w') as new_file:
+    new_file.write(random_text)
+
+with open('bar.txt') as new_file:
+    print(new_file.read())
